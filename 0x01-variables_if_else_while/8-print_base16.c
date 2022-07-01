cc91592597f600c7fc8c4025648318aca19d9003
NAttyXO <nattyxo2018@gmail.com>
2022-07-01 14:12:16 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char num[16] = "0123456789abcdef";
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(num[i]);
+	}
+	putchar('\n');
+	return (0);
+}
