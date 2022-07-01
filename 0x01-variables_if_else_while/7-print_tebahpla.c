a448b78e0aeb4dc6da458a8e42d9dbab1a55b20c
NAttyXO <nattyxo2018@gmail.com>
2022-07-01 13:51:57 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse, followed by a new line..
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char alp [26] = "zyxwvutsrqponmlkjihgfedcba";
+
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
