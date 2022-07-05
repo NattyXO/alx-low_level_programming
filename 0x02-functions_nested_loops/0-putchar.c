cee20c3f76218f55077ecd45b22c52ec6cf68223
NAttyXO <nattyxo2018@gmail.com>
2022-07-05 12:46:35 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints Holberton as a message.
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+    printf("_putchar\n");
+    return 0;
+}
