efc643a2aad2241fe30b4c8137b390de6425d5e6
NAttyXO <nattyxo2018@gmail.com>
2022-07-07 13:50:43 -0700

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - prints hashes squares.
+ * @size: size of the square.
+ * Return: no return.
+ */
+
+void print_diagonal(int n)
+{
+    int i, j;
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            _putchar(35);
+        }
+        _putchar('\n');
+
+    }
+    _putchar('\n');
+}
