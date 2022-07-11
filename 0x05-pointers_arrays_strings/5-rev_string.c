b83f401d27f217cdebebcc014b545104dfc71efd
NAttyXO <nattyxo2018@gmail.com>
2022-07-11 13:45:10 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,5 +7,13 @@
  */
 void rev_string(char *s)
 {
-  printf ("%s",strrev(s));
+    int i, len, temp;  
+    len = strlen(str1);
+    for (i = 0; i < len/2; i++)  
+    {  
+         
+        temp = str1[i];  
+        str1[i] = str1[len - i - 1];  
+        str1[len - i - 1] = temp;  
+    }
 }
