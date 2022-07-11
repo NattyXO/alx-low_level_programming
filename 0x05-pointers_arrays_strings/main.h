7adb31bab1be68ac65bddf6ee677388ea35110bd
NAttyXO <nattyxo2018@gmail.com>
2022-07-11 12:38:28 -0700

    0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,4 @@
 #ifndef main.h
-
 #define main.h
 
 void reset_to_98(int *n);
@@ -12,7 +11,4 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-
 #endif
