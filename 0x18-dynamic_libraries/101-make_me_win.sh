8002b22fc54479b55a3169b24cb119075a82618d
NattyXO <nattyxo2018@gmail.com>
2022-10-04 05:27:18 -0700

    3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/NattyXO/alx-low_level_programming/tree/main/0x18-dynamic_libraries
-export LD_PRELOAD="$PWD/../libinjection.so"
+wget -P /tmp/ https://github.com/NattyXO/alx-low_level_programming/tree/main/0x18-dynamic_libraries
+export LD_PRELOAD="$PWD/tmp/libinjection.so"
